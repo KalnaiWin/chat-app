5c6bc3399808fff8738924077ebfbd73d58f1956
Phúc <nhbphuc2006@gmail.com>
2025-09-28 10:58:26 +0700

    clean tree

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,12 +4,15 @@ import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config(); // allow using data from .env
 const app = express();
 const __dirname = path.resolve(); //  gets absolute path to backend folder
 
 const port = process.env.PORT || 3000;
+
+app.use(express.json()); // is a middleware that we calling to access field user sent by req.body
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
@@ -19,10 +22,18 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist"))); // tell express serve everything inside this folder
 
   app.get("/*splat", (_, res) => {
+    // => every api/__ will be display as html
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html")); // "../frontend/dist/index.html"
   });
 }
 
-app.listen(port, () => {
-  console.log("Server running on port: ", port);
-});
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log("Server running on port: ", port);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to the database", err);
+    process.exit(1);
+  });
